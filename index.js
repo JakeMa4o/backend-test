959791f6a0dd32970c1e65d3313c8cca21eedaa3
Jake Ma4o <2000_aktau@mail.ru>
2024-02-28 07:18:11 +0500

    MongoDB?

--- index.js
+++ index.js
@@ -1,22 +1,70 @@
-const express = require("express");
-const cors = require("cors");
+import dotenv from 'dotenv';
+import express, { response } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { Item } from './models/itemModel.js';
+
+
+dotenv.config()
 
 const app = express();
 app.use(express.json());
-
 app.use(cors());
 
+
+const connectionString = process.env.MONOGODB_URI || "";
+const port = process.env.PORT || 8000;
+
+
 app.get("/", (req, res) => {
-  res.status(200);
-  res.send("Welcome to express  ")
+  res.status(200).send("Welcome to express")
   // res.json({message: "Welcome to express"});
 })
 
+app.post("/items", async (req, res) => {
+  try {
+    if (
+      !req.body.name ||
+      !req.body.description
+    ) {
+      return res.status(400).send({
+        message: "Send all required fields name, description"
+      })
+    }
 
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+    const newItem = {
+      name: req.body.name,
+      description: req.body.description
+    };
+    const item = await Item.create(newItem);
+
+    return res.status(201).send(item);
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: error.message })
+  }
+})
 
 
+app.get("/items", async (req, res) => {
+  try {
+    const items = await Item.find({});
+    return res.status(200).json(items);
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send({ message: error.message })
+  }
+})
+
+mongoose
+.connect(connectionString)
+.then(() => {
+  console.log("App connected to database");
+  app.listen(port, () => console.log(`Listening to port ${port}`));
+})
+.catch((error) => {
+  console.log(error);
+});
 
 
 
